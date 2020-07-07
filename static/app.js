7ded6838333b3dee6c460c1eeb2c452e76d42921
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-07-08 01:08:47 +0200

    Control in the player movement using key arrows

--- static/app.js
+++ static/app.js
@@ -2,14 +2,50 @@ document.addEventListener('DOMContentLoaded', () => {
     // Get the squares
     const squares = document.querySelectorAll('.grid div');
     const startBtn = document.querySelector('.start');
-
     // Get the location of the player, door and monster
-    function getLocations() {
-    squares[player].classList.add('player')
+    let playerIndex = player
+    squares[playerIndex].classList.add('player')
     squares[door].classList.add('door')
     squares[monster].classList.add('monster')
+
+
+   function control(e) {
+
+       if (e.keyCode === 39) {
+           squares[playerIndex].classList.remove('player')
+           playerIndex += 1
+       }
+       else if (e.keyCode === 38) {
+           squares[playerIndex].classList.remove('player')
+         // Press up arrow
+         var corr = playerIndex.toString()
+         var y = corr[0] - 1
+         var z =  y + corr[1]
+         console.log(z);
+         playerIndex = parseInt(z)
+       }
+       else if (e.keyCode === 37) {
+           squares[playerIndex].classList.remove('player')
+         // Press left
+         playerIndex -= 1
+       }
+       else if (e.keyCode === 40) {
+           squares[playerIndex].classList.remove('player')
+         // Press down
+         var corr = playerIndex.toString()
+         var y = parseInt(corr[0]) + 1
+         var z = y.toString() + corr[1]
+         playerIndex = parseInt(z)
+       }
+
+       squares[playerIndex].classList.add('player')
+
+       console.log(playerIndex);
+
    }
 
-getLocations()
+   document.addEventListener('keyup', control)
+
+
 
 });
